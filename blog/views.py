3b2ee360e85b163f88cbaab94359b1c126838f5f
Ricardo Wojciechowski <ricardowoj@hotmail.com>
2020-08-11 22:33:34 -0300

    Delete a Blog Post

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 
 class ArticleDetailView(DetailView):
@@ -24,3 +26,9 @@ class UpdatePostView(UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'update_post.html'
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
